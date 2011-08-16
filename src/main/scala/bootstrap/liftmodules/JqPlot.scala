d90665f7e257d08b2b2dbf95ed1891f8ab60e2f7
Jonathan Ferguson <Jono@spiralarm.com>
2011-08-16 11:54:55 +1000

    Removed anything with references to ajax for the moment.

--- src/main/scala/bootstrap/liftmodules/JqPlot.scala
+++ src/main/scala/bootstrap/liftmodules/JqPlot.scala
@@ -13,16 +13,14 @@ package net {
     import net.liftweb.http.S
     import net.liftweb.common.Loggable
 
-      object JqPlot extends RestHelper with Loggable {
-
-        val m = Map[String,JqPlotAjax]();
+      object JqPlot extends Loggable {
         
         def init: Unit = {
           
           logger.info("JqPlot.init")
 
           ResourceServer.allow({
-            case "js" :: "jquery.js" :: Nil => true
+            case "js" :: "jquery.js" :: Nil => true   //This should be supplied.
             case "js" :: "jquery.jqplot.js" :: Nil => true
             case "js" :: "excanvas.js" :: Nil => true
             case "js" :: "plugins" :: "jqplot.barRenderer.js" :: Nil => true
@@ -53,23 +51,7 @@ package net {
             case "css" :: "jquery.jqplot.css" :: Nil => true
           })
 
-          serve {
-            case JsonGet("jqplot" :: id :: Nil, _) => 
-             logger.info("JqPlot.serve.jqPlot %s".format(id)) 
-             m.get(id) match {
-              case Some(plot) => 
-                
-                Full(JObject(List(JField("data",JString(plot.data.get.apply)),JField("options",JString(plot.options.get.apply)))))
-              case otherwise => Failure("Not yet implemented", Empty, Empty):Box[JValue]
-             }
-          }
-          
-          LiftRules.dispatch.append(JqPlot)
-
         }
-
-        def data(id:String,plot:JqPlotAjax) = {m.put(id,plot)}
-          
         
       }
       
@@ -104,53 +86,6 @@ package net {
         }
        
       } 
-    
-      
-      class JqPlotAjax(d: Box[() => String] , o: Box[() => String]) {
-
-        val data = d 
-        
-        val options = o 
-        
-        val id = Helpers.nextFuncName
-
-        JqPlot.data(id,this)
-        
-        
-        def toHtml = {
-	        val onLoad = JsRaw(
-"""$(document).ready(function(){
-  var json_url = "/jqplot/%s";
-  
-  var payload = (function () {
-        var ajaxResponse = '';
-        $.ajax({url:json_url, async:false, type:"get", success: function (data) {ajaxResponse = data;}, dataType:"json"}); 
-        return ajaxResponse;
-  }());
-
-  $.jqplot('%s', payload.data,payload.options);
-  
-});""".format(id, id))
-	
-
- 
-        <span>
-          <head>
-            <!--[if lt IE 9]><script language="javascript" type="text/javascript" src="/js/excanvas.js"></script><![endif]-->
-            <script type="text/javascript" src={S.contextPath + "/" + LiftRules.resourceServerPath + "/js/jquery.jqplot.js"}></script>
-            <script type="text/javascript" src={S.contextPath + "/" + LiftRules.resourceServerPath + "/js/plugins/jqplot.dateAxisRenderer.js"}></script>
-            <script type="text/javascript" src={S.contextPath + "/" + LiftRules.resourceServerPath + "/js/plugins/jqplot.canvasTextRenderer.js"}></script>
-            <script type="text/javascript" src={S.contextPath + "/" + LiftRules.resourceServerPath + "/js/plugins/jqplot.canvasAxisLabelRenderer.js"}></script>
-            <script type="text/javascript" src={S.contextPath + "/" + LiftRules.resourceServerPath + "/js/plugins/jqplot.trendline.js"}></script>
-            <link rel="stylesheet" type="text/css" href={S.contextPath + "/" + LiftRules.resourceServerPath + "/css/jquery.jqplot.css"} />
-            { Script(onLoad) }
-          </head>
-          <div id={ id } style="height:384px; width:512px;"></div>
-        </span>
-          
-        }
-       
-      }
 
     }
   }
