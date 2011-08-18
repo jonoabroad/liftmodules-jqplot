763d0c0d2ac64cdb5fc11c011ea190848a4a577c
Jonathan Ferguson <Jono@spiralarm.com>
2011-08-18 14:29:00 +1000

    A stab at plot options. Not sure.

--- src/main/scala/net/liftmodules/JqPlot.scala
+++ src/main/scala/net/liftmodules/JqPlot.scala
@@ -1,19 +1,18 @@
 package net {
   package liftmodules {
-      
-    import scala.collection.mutable.Map
-  import liftweb.common.{ Box, Empty, Failure,Full }
+  
+  import java.text.SimpleDateFormat    
+  
+  import scala.collection.mutable.Map
+  import scala.xml.NodeSeq
+  
+  import liftweb.common.{ Box, Empty, Failure,Full,Loggable }
   import liftweb.http.{ LiftRules, S,ResourceServer }
-  import liftweb.http.js.JE.JsRaw
+  import liftweb.http.js.JE.{JsRaw,JsVar}
   import liftweb.http.js.JsCmds._
+  import net.liftweb.json.JsonAST._
   import liftweb.util.{Helpers,Props}
   import liftweb.util.Props.RunModes._
-  import liftweb.common.Loggable
-  import scala.xml.NodeSeq
-  import net.liftweb.json.JsonAST._
-  import net.liftweb.http.js.JE.JsVar
-  import java.text.SimpleDateFormat
-
 
       object JqPlot extends Loggable {
         
@@ -36,7 +35,7 @@ package net {
       }
       
   
-      class JqPlot(w:Int,h:Int,options:String,series:List[List[Any]]*) extends Loggable {
+      class JqPlot(w:Int,h:Int,options:Box[Options],series:List[List[Any]]*) extends Loggable {
         
        val sdf =  new SimpleDateFormat("yyyy MM dd HH:mm")
         
@@ -49,21 +48,11 @@ package net {
             case Pilot => ".min.js"
             case otherwise => ".js"
         }
-        
-        val pluginsList = List( "barRenderer","BezierCurveRenderer","blockRenderer",
-            "bubbleRenderer","trendline","pointLabel","pieRenderer","ohlcRenderer",
-            "meterGaugeRenderer","mekkoRenderer", "logAxisRenderer","json2",
-            "highlighter","funnelRenderer","enhancedLegendRenderer","dragable",
-            "donutRenderer","dateAxisRenderer","canvasAxisLabelRenderer",
-            "canvasAxisTickRenderer","canvasOverlay","canvasTextRenderer",
-            "categoryAxisRenderer","ciParser","cursor")
 
-       val plugins = {
-          val lowerCaseOptions = options.toLowerCase()
-          for { 
-            plugin <- pluginsList
-            if lowerCaseOptions.indexOf("jqplot." + plugin.toLowerCase()) > -1
-          } yield <script type="text/javascript" src={"%s/%s/js/plugins/jqplot.%s%s".format(S.contextPath,LiftRules.resourceServerPath,plugin,version)}></script>
+       val plugins = options match {
+         case  Full(op) =>
+           		for { plugin <- op.plugins } yield <script type="text/javascript" src={"%s/%s/js/plugins/jqplot.%s%s".format(S.contextPath,LiftRules.resourceServerPath,plugin,version)}></script>
+         case otherwise => Nil   
         }     
         
         logger.info("plugins %s".format(plugins))
@@ -71,7 +60,7 @@ package net {
         def toCssTransformer:NodeSeq => NodeSeq = { _ => toHtml}
         
         def toHtml = {
-            val s = series.map { s => JArray( s.map { v => JArray( 
+            val jsonSeries = series.map { s => JArray( s.map { v => JArray( 
               v.map { 
               	case x:Int    => JInt(x)
               	case x:Double  => JDouble(x)
@@ -80,7 +69,17 @@ package net {
               }
               )
               })}.toList
-            val onLoadJs = OnLoad(JsRaw("var options = %s;".format(options)) & new JsCrVar("series",JArray(s)) & Run("$.jqplot('%s',series,options);".format(id)))
+              
+            val onLoadJs = {
+  
+                val y = new JsCrVar("series",JArray(jsonSeries))
+                val z = Run("$.jqplot('%s',series,options);".format(id))
+         
+                options match {
+                  case Full(o) =>  OnLoad( new JsCrVar("options",o.toJson) & y & z )
+                  case otherwise => OnLoad(new JsCrVar("options","") &  y & z ) 
+                }
+              }
         <span>
           <head_merge>
             <!--[if lt IE 9]><script language="javascript" type="text/javascript" src="/js/excanvas.js"></script><![endif]-->
@@ -92,7 +91,94 @@ package net {
           <div id={ id } style={ style }></div>
         </span>
         }
-      }   
+      }
+      
+      
+      class Options(title:Box[Title] = Empty,axes:Box[Axes] = Empty,series:List[Series] = Nil,legend:Box[Legend] = Empty,grid:Box[Grid] = Empty) {
+        
+                
+        private val pluginsList = List( "barRenderer","BezierCurveRenderer","blockRenderer",
+            "bubbleRenderer","trendline","pointLabel","pieRenderer","ohlcRenderer",
+            "meterGaugeRenderer","mekkoRenderer", "logAxisRenderer","json2",
+            "highlighter","funnelRenderer","enhancedLegendRenderer","dragable",
+            "donutRenderer","dateAxisRenderer","canvasAxisLabelRenderer",
+            "canvasAxisTickRenderer","canvasOverlay","canvasTextRenderer",
+            "categoryAxisRenderer","ciParser","cursor")
+        
+        
+        def plugins:List[String] = {Nil}
+        
+        def fields = List(title,axes,legend,grid)
+        
+        def toJson = { JObject(for { b <- fields; t <- b } yield t.toJson) } 
+                 
+      }
+
+      trait JSONable { 
+      
+        def fields:List[Box[JSONable]] = Nil
+        
+        def toJson:JField  
+        
+      }
+      
+      case class Title(name:String) extends JSONable  { override def toJson = JField("title",JString(name)) }
+      case class Axes(xaxis:Box[Axis] = Empty,yaxis:Box[Axis] = Empty,x2axis:Box[Axis] = Empty,y2axis:Box[Axis] = Empty)  extends JSONable {
+        
+        override def fields:List[Box[JSONable]] = List(xaxis,yaxis,x2axis,y2axis)
+        
+        def toJson = {JField("axes",JObject(for { b <- fields; t <- b } yield t.toJson))} 
+      }
+      sealed trait Renderer
+      case class LinearAxisRenderer extends Renderer { override def toString = "$.jqplot.LinearAxisRenderer" }
+      case class RenderOptions()  
+      case class TickOptions()  
+      case class Axis(min:Box[String] = Empty,max:Box[String] = Empty,pad:String,ticks:List[String] = Nil,numberTicks:Int,renderer:Renderer,rendererOptions:Box[RenderOptions] = Empty,
+          tickOptions:Box[TickOptions] = Empty,showTicks:Boolean = true,showTickMarks:Boolean = true) extends JSONable {
+                
+        override def fields:List[Box[JSONable]] = Nil//List(min,max,pad,ticks,numberTicks,renderer,renderOptions,tickOptions,showTicks,showTickMarks)
+        
+        override def toJson = {JField("axes",JObject(for { b <- fields; t <- b } yield t.toJson))}
+          
+      }
+      case class MarkerOption()  extends JSONable {
+
+        override def fields:List[Box[JSONable]] = Nil
+        
+        override def toJson = {JField("axes",JObject(for { b <- fields; t <- b } yield t.toJson))}        
+        
+      }
+      case class Series(lineWidth:Box[Int] = Empty,markerOptions:Box[MarkerOption] = Empty) extends JSONable {
+
+        override def fields:List[Box[JSONable]] = Nil
+        
+        override def toJson = { JField("axes",JObject(for { b <- fields; t <- b } yield t.toJson)) }        
+        
+      }  
+      sealed trait Location
+      case class NW() extends Location { override def toString = "NW" }
+      case class NO() extends Location  { override def toString = "N" }
+      case class NE() extends Location { override def toString = "NE" }
+      case class EA() extends Location  { override def toString = "E" }
+      case class SE() extends Location { override def toString = "SE" }
+      case class SO() extends Location  { override def toString = "S" }
+      case class SW() extends Location { override def toString = "SW" }
+      case class WE() extends Location  { override def toString = "W" }
+      case class Legend(location:Location,xoffset:Box[Int] = Empty,yoffset:Box[Int] = Empty)  extends JSONable{
+
+        override def fields:List[Box[JSONable]] = Nil
+        
+        override def toJson = { JField("axes",JObject(for { b <- fields; t <- b } yield t.toJson)) }        
+        
+      }
+      case class Grid(drawGridLines:Box[Boolean] = Empty,gridLineColor:String,background:String,borderColor:String,borderWidth:Box[Double] = Empty,shadow:Box[Boolean] = Empty,shadowAngle:Box[Int] = Empty,shadowOffset:Box[Double] = Empty,shadowWidth:Box[Int] = Empty,shadowDepth:Box[Int] = Empty,shadowAlpha:Box[Double] = Empty,renderer:Renderer,rendererOptions:Box[RenderOptions])  extends JSONable{
+
+        override def fields:List[Box[JSONable]] = Nil
+        
+        override def toJson = { JField("axes",JObject(for { b <- fields; t <- b } yield t.toJson)) }        
+        
+      }
+      
  
   }
 }
--- src/test/scala/net/liftmodules/JqPlotSpec.scala
+++ src/test/scala/net/liftmodules/JqPlotSpec.scala
@@ -2,19 +2,20 @@ package net {
   package liftmodules {
 
     import org.specs2.mutable._
+  import net.liftweb.common.Empty
 
     class JqPlotSpec extends Specification {
 
       "JqPlot" should {
         " correctly caculate plugins to add as Nil. " in {
-          val jqp = new JqPlot(0, 0, "options",List())
+          val jqp = new JqPlot(0, 0, Empty,List())
           
           jqp.plugins  must_== Nil
         }
         
         " correctly adds known plugins " in {
-          val jqp = new JqPlot(0, 0, """{seriesDefaults: {renderer: jQuery.jqplot.PieRenderer,rendererOptions: {showDataLabels: true}},legend: { show:true, location: 'e' }}""",List())
-          
+          val jqp = new JqPlot(0, 0, Empty,List())
+          //"""{seriesDefaults: {renderer: jQuery.jqplot.PieRenderer,rendererOptions: {showDataLabels: true}},legend: { show:true, location: 'e' }}"""
           jqp.plugins  must_== List(<script type="text/javascript" src="/classpath/js/jqplot.pieRenderer.js"></script>)
         }        
 
