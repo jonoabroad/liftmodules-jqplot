3f960c8559cf329fef41fcb0cc98c11fef7e645f
Jonathan Ferguson <Jono@spiralarm.com>
2011-08-29 17:28:56 +1000

    removed unused implicits.

--- src/main/scala/net/liftmodules/jqplot/PlotSnippet.scala
+++ src/main/scala/net/liftmodules/jqplot/PlotSnippet.scala
@@ -4,11 +4,6 @@ import net.liftweb.common._
 import net.liftweb.http.S
 
 trait PlotSnippet {
-	
-	implicit def pair2ListAny(p:List[(Any,Any)]):  List[List[Any]] = p.map{x => List(x._1,x._2)}
-	implicit def pair2ListAny(p:(Any,Any)):List[Any] = List(p._1,p._2)
-	implicit def series2Array(s:Series): Array[Series] = Array(s)
-	implicit def series2Array(o:Options): Box[Options] = Box !! o
 
     val w  = S.attr("w").map(a => a.toInt ).openOr(512)
     val h  = S.attr("h").map(a => a.toInt ).openOr(512)
